7c7e4c4e086b206f30795d860684caedd95262e8
henrique <henrique@xonha.space>
2024-01-24 19:14:25 -0300

    feat: search bar

--- src/pages/home/__init__.py
+++ src/pages/home/__init__.py
@@ -10,6 +10,8 @@ from src import templates
 
 router = APIRouter()
 
+yt = YTMusic()
+
 
 class Album(BaseModel):
     title: str
@@ -26,12 +28,40 @@ def removeFeatFromTracks(tracks: list[dict[str, Any]]) -> list[dict[str, Any]]:
     return tracks
 
 
+@router.get("/test")
+def test() -> None:
+    print("test")
+
+
+@router.get("/search")
+def search(request: Request):
+    search = request.query_params["search"]
+    search_results = yt.search(search, filter="albums")
+    context = {"request": request, "search_results": search_results}
+    return templates.TemplateResponse("home/components/search_results.html", context)
+
+
 @router.get("/")
 def home(
     request: Request,
     hx_request: Optional[str] = Header(None),
 ) -> HTMLResponse:
-    yt = YTMusic()
+    if hx_request:
+        album_id = request.query_params["browseId"]
+        album_raw = yt.get_album(album_id)
+        album = Album(
+            title=album_raw["title"].upper(),
+            artist=album_raw["artists"][0]["name"],
+            year=album_raw["year"],
+            cover_url=album_raw["thumbnails"][-1]["url"],
+            duration=album_raw["duration_seconds"],
+            tracks=removeFeatFromTracks(album_raw["tracks"]),
+        )
+
+        return templates.TemplateResponse(
+            "home/components/poster.html", {"request": request, "album": album}
+        )
+
     search_results = yt.search("Stoney", filter="albums")
     albumId = search_results[0]["browseId"]
     album_raw = yt.get_album(albumId)
@@ -45,8 +75,5 @@ def home(
     )
 
     context = {"request": request, "album": album}
-
-    if hx_request:
-        return templates.TemplateResponse("home/components/table.html", context)
 
     return templates.TemplateResponse("home/page.html", context)
